9623db88da37c0dee83118ae3ca45421dc3c874c
LRBeaver <GitPass16!>
2016-04-05 14:26:56 -0400

    working

--- Class_Test.py
+++ Class_Test.py
@@ -8,28 +8,40 @@ class Stock:
         self.current_price = current_price
         self.volatility = volatility
 
+    def list_stocks(self):
+        return ("Sock name is " + self.name + " and ticker is " + self.ticker + " and the price is " + str(self.starting_price))
+
 
 def create_market():
     global market
-    #numStocks = 5
+    numStocks = 5
     #for i in numStocks:
         #stock + i = Stock("")
-    stock1 = Stock("ABC", "ABC", 10, 10, 1)
-    stock2 = Stock("DEF", "DEF", 12, 12, 1.2)
-    stock3 = Stock("GHI", "GHI", 14, 14, 1.8)
-    stock4 = Stock("JKL", "JKL", 16, 16, 2.2)
-    stock5 = Stock("MNO", "MNO", 18, 18, 2.4)
-    print(stock1.name)
+    stock1 = Stock("American Broadcasting Corp", "ABC", 10, 10, 1)
+    stock2 = Stock("Diversified Energy Force", "DEF", 12, 12, 1.2)
+    stock3 = Stock("Global Health Inc", "GHI", 14, 14, 1.8)
+    stock4 = Stock("Jasmine Kiloton LLC", "JKL", 16, 16, 2.2)
+    stock5 = Stock("Minnesota Natural Order", "MNO", 18, 18, 2.4)
+    #def list_stocks(self):
+        #return("Sock name is " + self.name + " and ticker is " + self.ticker + " and the price is " + str(starting_price))
 
-def list_stocks():
-    #stock1
-    numstocks = 5
-    for i in range(numstocks):
-        print("stock" + str(i))
+    stock = Stock(name, ticker, starting_price, current_price, volatility)
+    for attr, value in stock.__dict__.iteritems():
+        print(attr, value)
+        #for i in range(numStocks):
+        #issue = "stock" + str(i)
+        #(issue.list_stocks())
+
+# def list_stocks():
+#     #stock1
+#     numstocks = 5
+#     for i in range(numstocks):
+#         print("stock" + str(i))
 
 def main():
     create_market()
-    list_stocks()
+    #stock1.list_stocks()
+    #list_stocks()
 
 main()
 
