a7238c0ee8abafb849b903335e9386b1af310290
LRBeaver <GitPass16!>
2016-04-06 08:47:00 -0400

    working

--- slass_two.py
+++ slass_two.py
@@ -11,20 +11,22 @@ class Stock:
     def list_stocks(self):
         return ("Stock name is " + self.name + " and ticker is " + self.ticker + " and the price is " + str(self.starting_price))
 
-    # #def __iter__(self, name, ticker, starting_price, current_price, volatility):
+    # def __iter__(self, name, ticker, starting_price, current_price, volatility):
     # stock = Stock(self, name, ticker, starting_price, current_price, volatility)
     # for attr, value in stock.__dict__.items():
     #     print(attr, value)
 
 def create_market():
     global market
-    numStocks = 5
+    #numStocks = 5
 
     stock1 = Stock("American Broadcasting Corp", "ABC", 10, 10, 1)
     stock2 = Stock("Diversified Energy Force", "DEF", 12, 12, 1.2)
     stock3 = Stock("Global Health Inc", "GHI", 14, 14, 1.8)
     stock4 = Stock("Jasmine Kiloton LLC", "JKL", 16, 16, 2.2)
     stock5 = Stock("Minnesota Natural Order", "MNO", 18, 18, 2.4)
+
+    market = {stock1, stock2, stock3, stock4, stock5}
 
     #stock = Stock(name, ticker, starting_price, current_price, volatility)
     #for attr, value in stock.__dict__.iteritems():
@@ -33,6 +35,7 @@ def create_market():
 
 def main():
     create_market()
+    print(market)
 
 
 main()
