57489ebeda7423efa926b1ac431bbeb5f00790a3
LRBeaver <GitPass16!>
2016-04-06 13:00:24 -0400

    working

--- Stock_Class_Two.py
+++ Stock_Class_Two.py
@@ -1,12 +1,12 @@
 import random as rnd
 
 class Stock:
-    def __init__(self, name, ticker, starting_price, current_price, volatility):
+    def __init__(self, name, ticker, starting_price, current_price):
         self.name = name
         self.ticker = ticker
         self.starting_price = starting_price
         self.current_price = current_price
-        self.volatility = volatility
+        #self.volatility = volatility
 
     def list_stocks(self):
         numStocks = 5
@@ -23,21 +23,29 @@ class Stock:
 global market
 #numStocks = 5
 
-stock1 = Stock("American Broadcasting Corp", "ABC", 10, 10, 1)
-stock2 = Stock("Diversified Energy Force", "DEF", 12, 12, 1.2)
-stock3 = Stock("Global Health Inc", "GHI", 14, 14, 1.8)
-stock4 = Stock("Jasmine Kiloton LLC", "JKL", 16, 16, 2.2)
-stock5 = Stock("Minnesota Natural Order", "MNO", 18, 18, 2.4)
+stock1 = Stock("American Broadcasting Corp", "ABC", 10, 10)
+stock2 = Stock("Diversified Energy Force", "DEF", 12, 12)
+stock3 = Stock("Global Health Inc", "GHI", 14, 14)
+stock4 = Stock("Jasmine Kiloton LLC", "JKL", 16, 16)
+stock5 = Stock("Minnesota Natural Order", "MNO", 18, 18)
 
+stock1.volatility = 1  # Add an 'age' attribute.
 #market = {stock1, stock2, stock3, stock4, stock5}
 
 
 stock1.list_stocks()
 #print(hasattr(stock1, 'ticker'))
 print(getattr(stock1, 'ticker'))
+print(hasattr(stock1, 'volatility'))
+print(getattr(stock1, 'volatility'))
 stock2.list_stocks()
 #print(hasattr(stock2, 'ticker'))
 print(getattr(stock2, 'ticker'))
+print(hasattr(stock2, 'volatility'))
+if (hasattr(stock2, 'volatility')):
+    print(getattr(stock2, 'volatility'))
+else:
+    pass
 stock3.list_stocks()
 #print(hasattr(stock3, 'ticker'))
 print(getattr(stock3, 'ticker'))
