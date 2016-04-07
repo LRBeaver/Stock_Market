6e6985e7582f60fe17986f2e92098f3b5aeb36ea
LRBeaver <GitPass16!>
2016-04-07 11:40:31 -0400

    Working Core

--- price_maker.py
+++ price_maker.py
@@ -1,16 +1,33 @@
 import random as rnd
-numStocks = 5
-
-for x in range(numStocks):
-    price = (rnd.randint(10 ,20))
 
 stockList = []
-stockList.append(Stock("American Broadcasting Corp", "ABC", price, 10))
-stockList.append(Stock("Diversified Energy Force", "DEF", price, 12))
-stockList.append(Stock("Global Health Inc", "GHI", price, 14))
-stockList.append(Stock("Jasmine Kiloton LLC", "JKL", price, 16))
-stockList.append(Stock("Minnesota Natural Order", "MNO", price, 18))
 
-print()
-for stock in stockList:
-    print("  ", stock.name, stock.ticker, stock.starting_price)
+class Stock:
+    def __init__(self, name, ticker, starting_price, current_price):
+        self.name = name
+        self.ticker = ticker
+        self.starting_price = starting_price
+        self.current_price = current_price
+        #self.volatility = volatility
+
+def fill_Market():
+    global stockList
+    nameList = ("American Broadcasting Corp", "Diversified Energy Force", "Global Health Inc", "Jasmine Kiloton LLC", "Minnesota Natural Order")
+    tickerList = ("ABC", "DEF", "GHI", "JKL", "MNO")
+
+    for i in range(len(nameList)):
+        stockName = nameList[i]
+        stockTicker = tickerList[i]
+        price = (rnd.randint(10 ,20))
+        stockList.append(Stock(stockName, stockTicker, price, price))
+
+def check_Market():
+    print()
+    for stock in stockList:
+        print("  ", stock.name, stock.ticker, stock.starting_price, stock.current_price)
+
+def main():
+    fill_Market()
+    check_Market()
+
+main()
